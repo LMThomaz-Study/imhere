83037030c7c2d7039739ecd11bd0af73d607790e
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-11 17:02:57 -0300

    init project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native'
 
-export default function App() {
+export function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: `#131016`,
+        padding: 24,
+      }}
+    >
+      <Text
+        style={{
+          color: `#fdfcfe`,
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48,
+        }}
+      >
+        Hello World!
+      </Text>
+      <Text
+        style={{
+          color: `#6b6b6b`,
+          fontSize: 16,
+        }}
+      >
+        Quarta, 11 de Janeiro de 2023.
+      </Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
