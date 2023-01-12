cd6030a5c51ea7b24fa0c0cdfa99d463788141b2
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-11 21:27:31 -0300

    Using StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,32 +1,28 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 export function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: `#131016`,
-        padding: 24,
-      }}
-    >
-      <Text
-        style={{
-          color: `#fdfcfe`,
-          fontSize: 24,
-          fontWeight: 'bold',
-          marginTop: 48,
-        }}
-      >
-        Hello World!
-      </Text>
-      <Text
-        style={{
-          color: `#6b6b6b`,
-          fontSize: 16,
-        }}
-      >
-        Quarta, 11 de Janeiro de 2023.
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Hello World!</Text>
+      <Text style={styles.eventDate}>Quarta, 11 de Janeiro de 2023.</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: `#131016`,
+    padding: 24,
+  },
+  eventName: {
+    color: `#fdfcfe`,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventDate: {
+    color: `#6b6b6b`,
+    fontSize: 16,
+  },
+})
