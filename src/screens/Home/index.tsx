d8c82ee196c08faaba8839c07c731c3d814ca5b8
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-05-10 13:40:19 -0300

    input data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 import { styles } from './styles'
 
 export function Home() {
@@ -6,6 +6,12 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Hello World!</Text>
       <Text style={styles.eventDate}>Quarta, 11 de Janeiro de 2023.</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome ddo participante"
+        placeholderTextColor="#6a6a6a"
+      />
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,17 +3,25 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: `#131016`,
+    backgroundColor: `#1a1a1a`,
     padding: 24,
   },
   eventName: {
-    color: `#fdfcfe`,
+    color: `#fafafa`,
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48,
   },
   eventDate: {
-    color: `#6b6b6b`,
+    color: `#6a6a6a`,
+    fontSize: 16,
+  },
+  input: {
+    padding: 16,
+    height: 56,
+    backgroundColor: '#1a1a1a',
+    borderRadius: 5,
+    color: '#fafafa',
     fontSize: 16,
   },
 })
